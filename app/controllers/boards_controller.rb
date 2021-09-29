9af899ac70a0f545a6cd38233616d98ca45d9c1b
Your Name <you@example.com>
2021-09-30 01:25:42 +0900

    投稿と時間format

--- app/controllers/boards_controller.rb
+++ app/controllers/boards_controller.rb
@@ -1,4 +1,21 @@
 class BoardsController < ApplicationController
   def index
+    @boards = Board.all
+  end
+
+  def new
+    @board = Board.new
+  end
+
+  def create
+    board = Board.create(board_params)
+    board.save
+    redirect_to boards_path
+  end
+
+  private
+
+  def board_params
+    params.require(:board).permit(:name, :title, :body)
   end
 end
