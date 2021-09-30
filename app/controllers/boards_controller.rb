bdd899e6e5742e2ee0130f719c989d6f07d9bcd3
Your Name <you@example.com>
2021-09-30 12:31:10 +0900

    flashメッセージ

--- app/controllers/boards_controller.rb
+++ app/controllers/boards_controller.rb
@@ -11,7 +11,7 @@ class BoardsController < ApplicationController
   def create
     board = Board.create(board_params)
     board.save
-    redirect_to boards_path
+    redirect_to board, flash: { notice: "「#{board.title}」の掲示板が作成されました。"}
   end
 
   def show
@@ -27,7 +27,7 @@ class BoardsController < ApplicationController
 
   def destroy
     @board.delete
-    redirect_to boards_path
+    redirect_to boards_path, flash: { notice: "「#{@board.title}」の掲示板が削除されました。"}
   end
 
   private
